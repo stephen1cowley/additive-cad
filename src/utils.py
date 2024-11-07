d4c339e99a4585a8c938e9c014215fb58d98bbf4
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2024-11-07 13:12:29 +0000

    pep compliant

--- src/utils.py
+++ src/utils.py
@@ -11,20 +11,23 @@ def normalize_answer(s: str) -> str:
     "Lower text and remove punctuation, articles and extra whitespace."
     def remove_articles(text):
         return re.sub(r'\b(a|an|the)\b', ' ', text)
+
     def white_space_fix(text):
         return ' '.join(text.split())
+
     def remove_punc(text):
         exclude = set(string.punctuation)
         return ''.join(ch for ch in text if ch not in exclude)
+
     def lower(text):
         return text.lower()
     return white_space_fix(remove_articles(remove_punc(lower(s))))
 
 
 def evaluate_em(
-        prediction: str,
-        answers: List[str],
-    ) -> bool:
+    prediction: str,
+    answers: List[str],
+) -> bool:
     "Exact match (EM) metric given a list of correct answers"
     norm_prediction = normalize_answer(prediction)
     norm_answers = [normalize_answer(ans) for ans in answers]
@@ -36,9 +39,9 @@ def evaluate_em(
 
 
 def evaluate_recall(
-        prediction: str,
-        answers: List[str],
-    ) -> bool:
+    prediction: str,
+    answers: List[str],
+) -> bool:
     "Recall metric (not exact match) given a list of correct answers"
     norm_prediction = normalize_answer(prediction)
     norm_answers = [normalize_answer(ans) for ans in answers]
