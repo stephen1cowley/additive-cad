ef4a57f6bc504607fc28fba89504174d0ef1be49
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2024-11-07 13:08:15 +0000

    save to config location

---
+++ run_experiment.py
@@ -0,0 +1,30 @@
+"""
+The run_experiment module: uses the config file to run the experiment.
+"""
+
+import os
+import json
+from src.additive_cad import AdditiveCad
+from src.experiment_types import ExperimentConfig
+
+if __name__ == "__main__":
+
+    directory = '/experiment_config'
+    file_names = os.listdir(directory)
+    file_names = [f for f in file_names if os.path.isfile(os.path.join(directory, f))]
+
+    # Take the first file in the /experiment_config directory as the config file
+    with open(file_names[0], "r") as f:
+        config_data = json.load(f)
+
+    config = ExperimentConfig.from_dict(config_data)
+    model = AdditiveCad(config)
+
+    # Run the experiment
+    results = model.generate_results()
+
+    # Write the results to the location specified by the config file
+    with open(config.output_file, 'w') as jf:
+        json.dump(results, jf)
+        print("----------")
+        print("Successfully finished the experiment")
