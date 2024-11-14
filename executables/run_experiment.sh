12efa18c911ee9c21f1fbbbd62d37f29bcc3e954
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2024-11-14 12:51:18 +0000

    Changed the source directory

--- executables/run_experiment.sh
+++ executables/run_experiment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source ~/pytorch-env/bin/activate
+source /rds/user/ssc42/hpc-work/pytorch-env/bin/activate
 export TRANSFORMERS_CACHE=/rds/user/ssc42/hpc-work
 
 python3 ../run_experiment.py --config $1
